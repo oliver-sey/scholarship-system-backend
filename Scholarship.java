0112068a6946583bec36686ed1beed7abb2a0f79
Katelyn <katelynrmclean@gmail.com>
2023-11-07 15:00:50 -0700

    Profile classes, started scholarship attributes

--- Scholarship.java
+++ Scholarship.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class Scholarship {
-	
+	private String name;
+    private ArrayList<StudentProfile> applicants = new ArrayList<StudentProfile>();
+    private DonorProfile Donor;
+    private float awardAmount;
+    
 }
