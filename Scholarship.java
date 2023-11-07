2ef9fe4b290832eb20a04d8d18fd175670888c69
Katelyn <katelynrmclean@gmail.com>
2023-11-07 15:39:38 -0700

    scholarship setters and getters

--- Scholarship.java
+++ Scholarship.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Scholarship {
 	private String name;
     private ArrayList<StudentProfile> applicants = new ArrayList<StudentProfile>();
     private DonorProfile Donor;
     private float awardAmount;
-    
+    private HashMap<String, String> requirements = new HashMap<String, String>();
+    private ArrayList<String> application = new ArrayList<String>();
+
+    //getters
+
+    public String getName(){
+		return this.name;
+	}
+
+    public float getAwardAmount() {
+        return this.awardAmount;
+    }
+
+    //setters
+
+	public void setName(String inputName) {
+		this.name = inputName;
+	}
+
+    public void setAwardAmount(float inputAwardAmount) {
+        this.awardAmount = inputAwardAmount;
+    }
 }
