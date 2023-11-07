c2e31699f44e4fa53ff63d054615b0180d7dc2b3
Anna Fravel <afravel@arizona.edu>
2023-11-07 14:34:22 -0700

    added password set/get

--- Profile.java
+++ Profile.java
@@ -2,11 +2,11 @@
  * An abstract class for a Profile, that will be the parent class for StudentProfile and more
  */
 public abstract class Profile {
-	String firstName;
-	String lastName;
-	String username;
+	private String firstName;
+	private String lastName;
+	private String username;
 	// TODO: is it ok that this is very low security storing the password as plain text??
-	String password;
+	private String password;
 
 	// Not sure what data type we should use for the answers to the security questions
 	// maybe a dictionary? I think the syntax would be Hashtable<String, String> my_dict = new Hashtable<String, String>();
@@ -28,6 +28,10 @@ public abstract class Profile {
 		return this.username;
 	};
 
+	public String getpassword(){
+		return this.password;
+	}
+
 	// TODO: will we need getters and setters for the password?
 	// remember that there might be restrictions on what values we want to allow the user to set their name, username, etc. to
 
@@ -43,5 +47,9 @@ public abstract class Profile {
 
 	public void setUsername(String inputUsername) {
 		this.username = inputUsername;
+	};
+
+	public void setPassword(String inputPassword) {
+		this.password = inputPassword;
 	};
 }
