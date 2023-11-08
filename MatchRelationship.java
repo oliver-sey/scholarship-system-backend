787fae29a2ad24ee3380ef780633e280e20f3f28
Katelyn <katelynrmclean@gmail.com>
2023-11-07 17:10:34 -0700

    match relationship setters and getters

--- MatchRelationship.java
+++ MatchRelationship.java
@@ -8,5 +8,49 @@ public class MatchRelationship {
     private HashMap<String, String> application = new HashMap<String, String>();
     private String applicationStatus;
     private HashMap<String, String> additionalRequirements = new HashMap<String, String>();
+    
+    //getters
+
+    public long getMatchPercentage(){
+        return this.matchPercentage;
+    }
+
+    public long getMatchIndex(){
+        return this.matchIndex;
+    }
+
+    public String getApplicationStatus(){
+        return this.applicationStatus;
+    }
+
+    public HashMap<String, String> getApplication() {
+        return this.application;
+    }
+
+    public HashMap<String, String> getAdditionalRequirements() {
+        return this.additionalRequirements;
+    }
+
+    //setters
+
+	public void setMatchPercentage(long inputMatchPercentage) {
+		this.matchPercentage = inputMatchPercentage;
+    }
+
+    public void setMatchIndex(long inputMatchIndex) {
+		this.matchIndex = inputMatchIndex;
+    }
+
+    public void setApplicationStatus(String inputApplicationStatus) {
+		this.applicationStatus = inputApplicationStatus;
+    }
+
+    public void setApplicationStatus(HashMap<String, String> inputApplication) {
+        this.application = inputApplication;
+    }
+
+    public void setAdditionalRequirements(HashMap<String, String> inputAdditionalRequirements) {
+        this.additionalRequirements = inputAdditionalRequirements;
+    }
 
 }
