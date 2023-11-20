892db534aab84ddcdbb405730fa5a42e0659ddc9
Oliver Seymour <oliver@seymourfam.com>
2023-11-20 15:31:24 -0700

    Added security questions and answers with getters and setters

--- Profile.java
+++ Profile.java
@@ -12,12 +12,12 @@ public abstract class Profile {
 	protected int clearanceLevel;
 	protected int fileIndex;
 
-	// Not sure what data type we should use for the answers to the security
-	// questions
-	// maybe a dictionary? I think the syntax would be Hashtable<String, String>
-	// my_dict = new Hashtable<String, String>();
+	// the questions and the answers for the 3 security questions
+	// setting the length to 3 because we know there will be 3 questions
+	protected String[] securityQuestions = new String[3];
+	protected String[] securityQAnswers = new String[3];
 
-	// Clearance Level?
+	// TODO: what do we do with clearance Level???
 
 	public String getFirstName() {
 		return this.firstName;
@@ -43,23 +43,74 @@ public abstract class Profile {
 		return this.fileIndex;
 	}
 
-	// TODO: will we need getters and setters for the password?
+	public String[] getAllSecurityQuestions() {
+		return this.securityQuestions;
+	}
+
+	/**
+	 * @param questionNum - the question number for which you want to get the
+	 *                    question (not the answer).
+	 *                    This should be 1, 2, or 3
+	 * @return the String text of the question that's at index (questionNum + 1)
+	 */
+	public String getOneSecurityQuestion(int questionNum) {
+		return this.securityQuestions[questionNum + 1];
+	}
+
+	public String[] getAllSecurityQAnswers() {
+		return this.securityQAnswers;
+	}
+
+	/**
+	 * @param questionNum - the question number for which you want to get the answer
+	 *                    This should be 1, 2, or 3
+	 * @return the String text of the answer that's at index (questionNum + 1)
+	 */
+	public String getOneSecurityQAnswer(int questionNum) {
+		return this.securityQAnswers[questionNum + 1];
+	}
+
 	// remember that there might be restrictions on what values we want to allow the
 	// user to set their name, username, etc. to
 
 	public void setFirstName(String inputFirstName) {
 		this.firstName = inputFirstName;
-	};
+	}
 
 	public void setLastName(String inputLastName) {
 		this.lastName = inputLastName;
-	};
+	}
 
 	public void setUsername(String inputUsername) {
 		this.username = inputUsername;
-	};
+	}
 
 	public void setPassword(String inputPassword) {
 		this.password = inputPassword;
-	};
+	}
+
+	public void setAllSecurityQuestions(String[] securityQuestions) {
+		this.securityQuestions = securityQuestions;
+	}
+
+	/**
+	 * @param questionNum - the question number for which you want to set the
+	 *                    question (not the answer).
+	 *                    This should be 1, 2, or 3
+	 */
+	public void setOneSecurityQuestion(int questionNum, String questionText) {
+		this.securityQuestions[questionNum + 1] = questionText;
+	}
+
+	public void setAllSecurityQAnswers(String[] securityQAnswers) {
+		this.securityQAnswers = securityQAnswers;
+	}
+
+	/**
+	 * @param questionNum - the question number for which you want to set the answer
+	 *                    This should be 1, 2, or 3
+	 */
+	public void setOneSecurityQAnswer(int questionNum, String answerText) {
+		this.securityQAnswers[questionNum + 1] = answerText;
+	}
 }
