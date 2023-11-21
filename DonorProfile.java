dc7b49ad99cdcc40a7528edee6ac45211d0b5c49
Katelyn <katelynrmclean@gmail.com>
2023-11-20 17:02:20 -0700

    added donor constructor back

--- DonorProfile.java
+++ DonorProfile.java
@@ -16,6 +16,13 @@ public class DonorProfile extends Profile {
         email = "N/A";
     }
 
+    public DonorProfile(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
     public String getPhoneNumber() {
         return this.phoneNumber;
     }
