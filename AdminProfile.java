8a27cede10b112dadace0ab909056ce7a732897f
mvogel521 <mileeavogel123@gmail.com>
2023-11-21 16:20:47 -0700

    added default constructor for adminprofile

--- AdminProfile.java
+++ AdminProfile.java
@@ -1,5 +1,10 @@
 public class AdminProfile extends Profile {
+    // create constructor for AdminProfile child class
     public AdminProfile() {
+        firstName = "invalidFirstName";
+        lastName = "invalidLastName";
+        username = "invalidUserName";
+        password = "invalidPassword";
         clearanceLevel = 4;
     }
 }
