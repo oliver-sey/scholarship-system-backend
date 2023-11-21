c1d71aa05dd9541e55fc094c4608b3f6c885d3aa
mvogel521 <mileeavogel123@gmail.com>
2023-11-21 16:17:36 -0700

    added default constructors for fundsteward/donor

--- DonorProfile.java
+++ DonorProfile.java
@@ -3,7 +3,17 @@ import java.util.ArrayList;
 public class DonorProfile extends Profile {
     ArrayList<Scholarship> scholarships = new ArrayList<Scholarship>();
 
+    // create constructor for DonorProfile child class
+    public DonorProfile(){
+        clearanceLevel = 2;
+        firstName = "invalidFirstName";
+        lastName = "invalidLastName";
+        username = "invalidUserName";
+        password = "invalidPassword";   
+    }
+
     public DonorProfile(String firstName, String lastName, String username, String password) {
+        clearanceLevel = 1;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
--- FundStewardProfile.java
+++ FundStewardProfile.java
@@ -1,10 +1,18 @@
 public class FundStewardProfile extends Profile {
+    // create constructor for FundSteward child class
+    public FundStewardProfile() {
+        clearanceLevel = 3;
+        firstName = "invalidFirstName";
+        lastName = "invalidLastName";
+        username = "invalidUserName";
+        password = "invalidPassword";
+    }
 
     public FundStewardProfile(String firstName, String lastName, String username, String password) {
+        clearanceLevel = 3;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
     }
-
 }
