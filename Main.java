5b76f6e5dd0c3337e36a619287bac4c0206e2362
Oliver Seymour <oliver@seymourfam.com>
2023-11-22 21:13:48 -0700

    Added userRunSystem(), oneUserAction()

--- Main.java
+++ Main.java
@@ -10,10 +10,106 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		BackendSystem backend = new BackendSystem();
+		// userRunSystem will prompt them to log in
+		// userRunSystem(backend);
+
 		runDifferentTests();
 
 	}
 
+	// prompt the user to log in and get their 
+	public static void userRunSystem(BackendSystem backend) {
+		// prompt the user to log in, when this line of code is done they are successfully logged in or the system should stop
+		boolean successfulLogin = backend.login();
+
+		if (successfulLogin) {
+			String userInput = "";
+			do {
+				// prompt them for one action
+				oneUserAction(backend);
+
+				// see if they want to quit
+				Scanner scnr = new Scanner(System.in);
+				System.out.print("Enter 'q' to quit, or type anything else to keep going: ");
+				userInput = scnr.next();
+			} while (userInput != "q");
+		}
+		// else just quit
+	}
+
+
+	/**
+	 * You should get the user to log in before calling this!
+	 * prompts the user for which action they want to perform based on their profile type
+	 */
+	public static void oneUserAction(BackendSystem backend) {
+		Scanner scnr = new Scanner(System.in);
+		int userSelection = -1;
+		// TODO: should we use clearance level?
+		if (backend.getCurrentUser() instanceof StudentProfile) {
+			// TODO: implement this!!
+			System.out.println("Have to still implement oneUserAction() for StudentProfile.");
+		}
+		else if (backend.getCurrentUser() instanceof DonorProfile) {
+			// TODO: implement this!!
+			System.out.println("Have to still implement oneUserAction() for DonorProfile.");
+		}
+		else if (backend.getCurrentUser() instanceof StaffProfile) {
+			// TODO: implement this!!
+			System.out.println("Have to still implement oneUserAction() for StaffProfile.");
+		}
+		else if (backend.getCurrentUser() instanceof FundStewardProfile) {
+			// TODO: implement this!!
+			System.out.println("Have to still implement oneUserAction() for FundStewardProfile.");
+		}
+		else if (backend.getCurrentUser() instanceof AdminProfile) {
+			// TODO: implement this!!
+			System.out.println("Options: ");
+			System.out.println("0 - view unapproved scholarships to approve them");
+
+			System.out.print("Your selection: ");
+			userSelection = scnr.nextInt();
+
+			// if they want to view unapproved scholarships to approve them
+			if (userSelection == 0) {
+				System.out.println("Unapproved scholarships:");
+				// get all the unapproved scholarships and print a number and the scholarship name and description
+				ArrayList<Scholarship> unapprovedSchols = new ArrayList<Scholarship>();
+				for (int i = 0; i < unapprovedSchols.size(); i++) {
+					System.out.println((i + 1) + ": " + unapprovedSchols.get(i).getName());
+					System.out.println(unapprovedSchols.get(i).getDescription());
+					System.out.println();
+				}
+
+				// prompt them to approve one scholarship
+
+				int scholNumToApprove = -1;
+				do {
+					System.out.print("Please enter a scholarship number that you want to approve, or -1 if you are done: ");
+					scholNumToApprove = scnr.nextInt();
+
+					// if they want to approve a scholarship
+					if (scholNumToApprove != -1) {
+						// TODO: will this go all the way through the chain of pointers and actually change the original scholarship object????
+						System.out.println("Setting the scholarship to approved");
+						unapprovedSchols.get(scholNumToApprove).setApproved(true);
+					}
+				} while(scholNumToApprove != -1);
+			}
+
+			else if (userSelection == 1) {
+				System.out.println("Have to still write code for userSelection == 1 for AdminProfile in oneUserAction()");
+			}
+		}
+		else {
+			// should never get here
+			System.out.println("Invalid user type in userOptions");
+		}
+		
+		
+
+	}
 
 	public static void runDifferentTests() throws Exception {
 		Scanner scnr = new Scanner(System.in);
@@ -31,6 +127,7 @@ public class Main {
 			System.out.println("4 - test updateDonorProfileFile()");
 			System.out.println("5 - print all of one thing, can customize");
 			System.out.println("6 - test storeNewDonorProfile()");
+			System.out.println("7 - login as an admin and then approve scholarships");
 			System.out.println("0 - EXIT");
 
 			System.out.print("\nYour choice: ");
@@ -108,9 +205,9 @@ public class Main {
 			else if (userSelection == 5) {
 				BackendSystem backend = new BackendSystem();
 				// System.out.println("all donors: " + backend.getAllDonors());
-				System.out.println("all scholarship names: ");
-				for (Scholarship scholarship : backend.getAllScholarships()) {
-					System.out.println("'" + scholarship.getName() + "'");
+				System.out.println("all admin names: ");
+				for (AdminProfile admin : backend.getAllAdmins()) {
+					System.out.println("'" + admin.getName() + "'");
 				}
 			}
 			
@@ -124,6 +221,10 @@ public class Main {
 				backend.storeNewDonorProfile(donorToCopy);
 				System.out.println("Made new folder and files for that donor, the folder name should be /donors/donor" + (backend.findNextFileIndex("donor") - 1));
 				System.out.println("!!!! Please be sure to delete that folder so we don't have duplicates.");
+			}
+
+			else if (userSelection == 7) {
+				// TODO: fill this out
 			}
 
 			else {
